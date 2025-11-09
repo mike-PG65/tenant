9fb5d84d8f96c5971bcf3be6c041832a62297e9b
mike-PG65 <petergonye123@gmail.com>
2025-11-09 20:31:04 +0300

    updated the handle download function

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         gray: {
           100: '#f3f4f6',
+          200: '#e5e7eb',
           300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
           600: '#4b5563',
           700: '#374151',
           800: '#1f2937',
@@ -17,9 +18,5 @@ export default {
         yellow: { 600: '#ca8a04' },
       },
     },
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
-    disableColorOpacityUtilitiesByDefault: true,
   },
 };
